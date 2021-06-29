ee6b05842eaec493ad9ee00119e4899d6d480093
v4hn <me@v4hn.de>
2021-06-29 10:00:28 +0200

    filter invalid lfj2 from joint state

--- tams_motorhand/scripts/filter_invalid_joint_states.py
+++ tams_motorhand/scripts/filter_invalid_joint_states.py
@@ -1,27 +1,37 @@
 #!/usr/bin/env python
 
-# a small helper script that sets rh_MFJ4 to 0/0/0 in the published joint_states
-# This is useful when position sensing returns invalid data for the joint.
-# v4hn @ 20180704
+# a small helper script that sets invalid joints to 0/0/0 in the published joint_states
+# This is useful when position sensing returns invalid data for a joint.
+# v4hn @ 20180704 for mfj4
+# v4hn @ 20210629 use for lfj2
 
 import rospy
 from sensor_msgs.msg import JointState
 
 pub= None
 
-def filter(msg):
+INVALID_JOINTS= [
+#    'rh_MFJ4',
+    'rh_LFJ2',
+]
+
+def zero_invalid_joints(msg):
     global pub
-    idx=msg.name.index('rh_MFJ4')
+
     msg.position= list(msg.position)
     msg.velocity= list(msg.velocity)
     msg.effort= list(msg.effort)
-    msg.position[idx]= 0.0
-    msg.velocity[idx]= 0.0
-    msg.effort[idx]= 0.0
+
+    for j in INVALID_JOINTS:
+        idx=msg.name.index(j)
+        msg.position[idx]= 0.0
+        msg.velocity[idx]= 0.0
+        msg.effort[idx]= 0.0
+
     pub.publish(msg)
 
 if __name__ == '__main__':
-    rospy.init_node('mfj4_joint_state_filter')
+    rospy.init_node('invalid_joint_state_filter')
     pub= rospy.Publisher('joint_states', JointState, queue_size=1)
-    sub= rospy.Subscriber('joint_states_original', JointState, filter, queue_size=1)
+    sub= rospy.Subscriber('joint_states_original', JointState, zero_invalid_joints, queue_size=1)
     rospy.spin()
